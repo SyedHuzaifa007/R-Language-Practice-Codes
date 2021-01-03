4c3b9266fe4316811dca3a6797b91c4513c201b0
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-03 12:48:37 -0800

    made and updated comments.R

---
+++ comments.R
@@ -0,0 +1,15 @@
+print('Lets start another topic of how to comment code in Rebol language')
+print('This is also very simialr to Python Programming Lanuage')
+print("All you have to do is to place a '#' sign before the line. Interpreter will ignore this line and will move forward")
+# It is used to enhance the readability of the code
+# Also you can use it to memorize that for what purpose i have wrote this code
+# Recalling Python the same commenting method was used there
+print("As we know that Python Programming Language also provide us the facility of multiline comment")
+print("Like Python Programming Language Rebol does not provide us the facility of multiline comment")
+print("Insted you can cope this issue by writing like this")
+# I know you'll be laughing seeing this
+# But this is the only way to make multiline comment
+# Anyway be happy that this is very similar to Python Programming Lanuage
+# You'll be remembering that we used '''triple quotes''' for multiline comment in python
+# But trust me it will not show the same output here 
+print("Clone it and practice yourself as practice makes the man perfect.")
