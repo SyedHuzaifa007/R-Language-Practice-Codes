c8412057bbaae7fdcea622fc789be91f7b039556
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-03 13:05:19 -0800

    made variables.R and updated it

---
+++ variables.R
@@ -0,0 +1,14 @@
+print("Lets start one of the most important topic of Programming that is variables")
+print("Variables are the storage spaces in the programming language that are used to store results and data")
+print("As we are relating the Rebol Language with the Python Proramming Language")
+print("Because most of the syntax of Rebol Language is very similar to the Python Programming Language")
+print("As you know that in Python programming language we declare the variable by following syntax")
+print("a = 'Hello World!'")
+print("But in Rebol Language follpwing syntax is used")
+a <- "Hello World!"
+print(a)
+b <- 100
+print(b)
+print("Did you see what I am seeing")
+print("Instead of using equals to '=' we use '<-' in Rebol Programming Language")
+print("Practice this topic yourself and create at least 15 variables that stores different types of data")
