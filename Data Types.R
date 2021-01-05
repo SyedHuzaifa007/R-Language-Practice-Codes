0c98c0454be127b73afbf7284f42290d0ed5457f
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-05 00:29:38 -0800

    created Data Types.R

---
+++ Data Types.R
@@ -0,0 +1,7 @@
+print("Now let check what are datatypes in Rebol Language")
+print("I am going to print a list of data types in R")
+print("1. Numeric e.g (100,10,8.5) etc")
+print("2. Integer e.g (100L,10L,8L) etc where letter 'L' declare these values as integer")
+print("3.Complex e.g (8 + 5i) etc where 'i' is the imaginary part")
+print('4.Characters/String e.g ("Rebol", "Hello","World!") etc')
+print("5.Logical/Booleans e.g (TRUE or FALSE) etc")
