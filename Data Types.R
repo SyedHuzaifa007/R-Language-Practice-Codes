ad771650824760ca9c171f672e1e19ac1aa06d25
SyedHuzaifa007 <68459184+SyedHuzaifa007@users.noreply.github.com>
2021-01-05 11:05:55 -0800

    updated Data Types.R

--- Data Types.R
+++ Data Types.R
@@ -5,3 +5,15 @@ print("2. Integer e.g (100L,10L,8L) etc where letter 'L' declare these values as
 print("3.Complex e.g (8 + 5i) etc where 'i' is the imaginary part")
 print('4.Characters/String e.g ("Rebol", "Hello","World!") etc')
 print("5.Logical/Booleans e.g (TRUE or FALSE) etc")
+print("Now I am going to check the data types practically")
+a <- "Name"
+print("Class function is used to check the Data Type In Rebol Language")
+print(class(a))
+b <- 100L
+print(class(b))
+c <- 8 + 5i
+print(class(c))
+d <- 10
+print(class(d))
+e <- TRUE
+print(class(e))
